194dab479ab933db5b342c7f7da40cec51448e9b
diamond-nicholas <diamondnicholas154@gmail.com>
2021-03-08 15:44:20 +0100

    fix linters and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create edit update]
 
-  get 'user/create' => 'users#create'
-  # get "users/:id/edit", to: "users#edit"
-  # patch "user/:id",to: "users#update"
+  get "/users" => 'users#create', as: :edit
+  root "users#create"
+  
 end
