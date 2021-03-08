8db56722502b14eced27e34d976971c34395e8d3
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-03-08 17:27:34 -0500

    Edited config/routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@
 
 Rails.application.routes.draw do
   resources :users, only: %i[new create edit update]
-
-  get "/users" => 'users#create', as: :edit
-  root "users#create"
+  
+  root "users#new"
   
 end
